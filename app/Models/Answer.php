bb162aa4c62cd27d8e795d14ba7cf5f892adca10
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-24 20:29:43 +0800

    Create Answer model with relationships to Question and UserProgresses
    - Implemented `Answer` model with relationships to `Question` and `UserProgress`.
    - Included boolean casting for `is_correct` field.

---
+++ app/Models/Answer.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'question_id',
+        'label',
+        'content',
+        'image_src',
+        'image_alt',
+        'is_correct',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_correct' => 'boolean',
+    ];
+
+    /**
+     * Get the question that owns the answer.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    /**
+     * Get the user progresses for the answer.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function userProgresses()
+    {
+        return $this->hasMany(UserProgress::class);
+    }
+}
