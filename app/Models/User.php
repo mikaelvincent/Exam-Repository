b7b98a5bcfa19527cb778fc5ceee03f3ba0c0f38
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-24 20:29:04 +0800

    Create User model with relationships and registered user check
    - Implemented `User` model with `userProgresses` and `httpRequests` relationships.
    - Added method `isRegistered()` to differentiate between registered and unregistered users.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,29 +2,34 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
+     * For unregistered users, only 'uuid' is required.
+     * For registered users, additional fields are filled.
+     *
      * @var array<int, string>
      */
     protected $fillable = [
+        'uuid',
         'name',
         'email',
         'password',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for arrays.
+     *
+     * Hides sensitive fields like 'password' and 'remember_token'.
      *
      * @var array<int, string>
      */
@@ -36,9 +41,41 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast.
      *
+     * Casts 'email_verified_at' to datetime.
+     *
      * @var array<string, string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the user progresses for the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function userProgresses()
+    {
+        return $this->hasMany(UserProgress::class);
+    }
+
+    /**
+     * Get the HTTP requests for the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function httpRequests()
+    {
+        return $this->hasMany(HttpRequest::class);
+    }
+
+    /**
+     * Determine if the user is registered.
+     *
+     * @return bool
+     */
+    public function isRegistered()
+    {
+        return !is_null($this->email);
+    }
 }
