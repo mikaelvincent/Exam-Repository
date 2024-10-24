c5eafd8d69390aeb4c1cfd4ac31144468f4cbff3
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-24 22:48:29 +0800

    Add HttpRequestFactory to generate random HTTP request data
    - Created a factory for the HttpRequest model to simulate user-related HTTP requests with various methods and paths.

---
+++ database/factories/HttpRequestFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\HttpRequest>
+ */
+class HttpRequestFactory extends Factory
+{
+    protected $model = \App\Models\HttpRequest::class;
+
+    public function definition()
+    {
+        $httpMethods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS', 'TRACE', 'CONNECT'];
+
+        return [
+            'user_id' => User::factory(),
+            'http_method' => $this->faker->randomElement($httpMethods),
+            'path' => $this->faker->url(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
