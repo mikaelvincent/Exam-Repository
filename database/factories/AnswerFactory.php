b4372e581edb08de47d02a45a30b2b383365fc3d
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-25 19:50:05 +0800

    Make answer content and labels nullable in factory
    - Updated the AnswerFactory to allow the 'content' and 'label' attributes to be NULL by using the faker's optional() method.

--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -15,8 +15,8 @@ class AnswerFactory extends Factory
     public function definition()
     {
         return [
-            'label' => strtoupper($this->faker->randomElement(['A', 'B', 'C', 'D'])),
-            'content' => $this->faker->sentence(),
+            'label' => $this->faker->optional()->randomElement(['A', 'B', 'C', 'D']),
+            'content' => $this->faker->option()->sentence(),
             'image_src' => $this->faker->optional()->imageUrl(),
             'image_alt' => $this->faker->optional()->words(3, true),
             'is_correct' => $this->faker->boolean(25), // 25% chance of being correct
