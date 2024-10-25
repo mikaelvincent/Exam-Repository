7bf3a29026dd67dedb609019ab87fb8153bb4766
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-26 02:29:57 +0800

    Reorder middleware to ensure EncryptCookies runs before GuestUserMiddleware
    - Moved EncryptCookies middleware above GuestUserMiddleware in the 'web' middleware group to ensure that cookies are decrypted before being accessed.
    
    This resolves the issue where the guest_uuid cookie was not being properly decrypted, causing new UUIDs to be generated on each page refresh.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,8 +30,8 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \App\Http\Middleware\GuestUserMiddleware::class,
             \App\Http\Middleware\EncryptCookies::class,
+            \App\Http\Middleware\GuestUserMiddleware::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
