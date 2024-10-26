ee3157c1bd38fcc6a75128b55c271ecd2c00a6ed
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-26 22:41:16 +0800

    Register LogHttpRequests middleware
    - Added LogHttpRequests middleware to the global HTTP middleware stack.
    - Ensures all incoming HTTP requests are logged consistently.
    - Facilitates tracking of user interactions and request patterns.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,8 +9,6 @@ class Kernel extends HttpKernel
     /**
      * The application's global HTTP middleware stack.
      *
-     * These middleware are run during every request to your application.
-     *
      * @var array<int, class-string|string>
      */
     protected $middleware = [
@@ -21,6 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\LogHttpRequests::class,
     ];
 
     /**
@@ -48,8 +47,6 @@ class Kernel extends HttpKernel
 
     /**
      * The application's route middleware.
-     *
-     * These middleware may be assigned to groups or used individually.
      *
      * @var array<string, class-string|string>
      */
