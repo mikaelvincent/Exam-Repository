6c47363cfb901807169e272f3a4ca76e26d17c97
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-26 18:31:37 +0800

    Update web routes to use content resolution middleware
    - Applied 'content.resolution' middleware to relevant routes
    - Adjusted routes to handle exam sets and questions dynamically
    - Removed ContentResolverController in favor of middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContentResolverController;
+use App\Http\Controllers\ExamSetController;
+use App\Http\Controllers\QuestionController;
 
 Route::get('/', function () {
     return view('home');
@@ -16,5 +17,20 @@ Route::get('/contributors', function () {
     return view('pages.contributors', compact('breadcrumbs'));
 });
 
-Route::get('/{any}', [ContentResolverController::class, 'resolve'])
-    ->where('any', '.*');
+Route::middleware('content.resolution')->group(function () {
+    Route::get('/{examSetSlug1}/{examSetSlug2?}/{examSetSlug3?}/{examSetSlug4?}/{questionSlug?}', function () {
+        $examSet = request()->get('examSet');
+        $question = request()->get('question');
+        $breadcrumbs = request()->get('breadcrumbs');
+
+        if ($question) {
+            return view('question', compact('question', 'breadcrumbs'));
+        }
+
+        if ($examSet) {
+            return view('exam_set', compact('examSet', 'breadcrumbs'));
+        }
+
+        return abort(404, 'Content not found');
+    })->where('any', '.*');
+});
